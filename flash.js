436902aa42f5a4d22bbe3e881dc4b6a3f47299eb
manatago <satoshi@tagomori.biz>
2020-07-12 13:50:42 +0900

    test

--- flash.js
+++ flash.js
@@ -9,14 +9,20 @@ $(function(){
 	var d = new Array;
 	//初期値
 	var start;
+	var reset;
 	var num = 0;
 	var interval_id;
 	var disp;
+	var timeout_id;
 	var interval_time=4000;
 	if($('#flash_card').attr('interval')){
 		interval_time=Number($('#flash_card').attr('interval'));
 	}
-	var fsize=45;
+	var fsize=60;
+	if($('#flash_card').attr('fsize')){
+		fsize=Number($('#flash_card').attr('fsize'));
+		//console.log(fsize);
+	}
 	//指定ファイルを読み込む
 	get_data(file_name);
 	//TSVファイルを配列に変換
@@ -25,7 +31,8 @@ $(function(){
 	put_audios();
 	//STYLEを指定
 	flash_card_style();
-	console.log(d);
+	//実行中かどうかのフラグ
+	var start_flag = false;
 
 	function put_audios(){
 		for(var i=0;i<d.length;i++){
@@ -40,41 +47,77 @@ $(function(){
 	function flash_card_style(){
 		$('#flash_card').parent().css('width','100%');
 		$('#flash_card').css({
-											'max-width':'500px',
-											'height':'280px',
+											'width':'100%',
 											'padding':'0',
-											'background-image':'url(https://support.gakuto.co.jp/wp-content/uploads/2020/06/flash_card.png)',
-											'background-repeat':'no-repeat',
-											'background-size':'contain',
 											'margin':'0 auto'
 										});
 		//表示エリアの追加
 		disp = $('<div>');
 		disp.css({
-							'max-width':'500px',
+							'width':'100%',
 							'margin':'0 auto',
-							'padding':'15px 0 0 25%',
-							'height':'150px',
-							'font-size':fsize+'px'
+							'padding':'15px',
+							'min-height':'192px',
+							'font-size':fsize+'px',
+							'border':'solid 2px #cccccc',
+							'position':'relative'
 		});
-		disp.on('click',function(){
-			clearInterval(interval_id);
-			start.prop('disabled',false);
-		});
+		//disp.on('click',function(){
+		//	clearInterval(interval_id);
+		//	start.prop('disabled',false);
+		//});
 		$('#flash_card').append(disp);
 		$('#flash_card').css('padding','10 20px');
+
+		add_start();
+		add_reset();
+	}
+
+
+	function add_reset(){
+		//ボタンの追加
+		reset = $('<button>');
+		reset.html('リセット');
+		reset.css('display','block');
+		reset.css('margin-left','auto');
+		reset.css('width','50%');
+		reset.css('border-radius','20px');
+		$('#flash_card').append(reset);
+		reset.on('click',function(){
+				start_flag=false;
+				clearInterval(interval_id);
+				num=0;
+				start.html('再生');
+				disp.html('');
+		});
+	}
+
+	function add_start(){
 		//ボタンの追加
 		start = $('<button>');
 		start.html('再生');
 		start.css('display','block');
 		start.css('margin-left','auto');
 		start.css('width','50%');
+		start.css('margin-top','30px');
+		start.css('margin-bottom','30px');
 		start.css('border-radius','20px');
 		$('#flash_card').append(start);
 		start.on('click',function(){
-				flash_start();
+				if(start_flag===false){
+					flash_start();
+				}else{
+					console.log('stop');
+					console.log(timeout_id);
+					console.log(clearTimeout(timeout_id));
+					//clearInterval(interval_id);
+					start.html('再生');
+					start_flag=false;
+				}
 		});
 	}
+
+/*
 	function display_flash(){
 		var this_num=num;
 		//音声再生
@@ -82,10 +125,13 @@ $(function(){
 		audio.play();
 		//英語を表示
 		resize_font(d[this_num][0],'en');
-		disp.html(d[this_num][0]);
+		var st='position:absolute;bottom:0';
+		disp.html("<div style='"+st+"'>"+d[this_num][0]+"</div>");
 		//遅れて日本語を表示
 		setTimeout(function(){
-			disp.html(d[this_num][2]);
+			if(start_flag===true){
+				disp.html("<div style='"+st+"'>"+d[this_num][2]+"</div>");
+			}
 			resize_font(d[this_num][2],'ja');
 		},interval_time/2);
 		num++;
@@ -95,37 +141,82 @@ $(function(){
 			start.prop('disabled',false);
 		}	
 	}
+*/
 
 	//フォントのリサイズ
 	function resize_font(text,lang){
+		var r = 6;
+		var e=1;
+		var w=$(window).width();
+		if(w<500){
+			r =r *1;
+		}else if(w<1000){
+			r=r*1.5;
+		}else{
+			r=r*1.7;
+		}
+
 		if(lang==='ja'){
 			if(text.length<6){
-				disp.css('font-size',fsize+'px');
+				disp.css('font-size',fsize/r+'vw');
 			}else if(text.length<8){
-				disp.css('font-size',fsize*0.8+'px');
+				disp.css('font-size',fsize*0.9/r+'vw');
 			}else if(text.length<10){
-				disp.css('font-size',fsize*0.7+'px');
+				disp.css('font-size',fsize*0.8/r+'vw');
 			}else{
-				disp.css('font-size',fsize*0.5+'px');
+				disp.css('font-size',fsize*0.7/r+'vw');
 			}
 		}else if(lang==='en'){
 			if(text.length<12){
-				disp.css('font-size',fsize+'px');
+				disp.css('font-size',fsize/(r/e)+'vw');
 			}else if(text.length<16){
-				disp.css('font-size',fsize*0.8+'px');
+				disp.css('font-size',fsize*0.9/(r/e)+'vw');
 			}else if(text.length<20){
-				disp.css('font-size',fsize*0.7+'px');
+				disp.css('font-size',fsize*0.8/(r/e)+'vw');
 			}else{
-				disp.css('font-size',fsize*0.5+'px');
+				disp.css('font-size',fsize*0.7/(r/e)+'vw');
 			}
 		}
 	}
 
 	function flash_start(){
-		display_flash();
-		start.prop('disabled',true);
-		interval_id = setInterval(display_flash,interval_time);	
+		start.html('停止');
+		start_flag=true;
+		//display_flash();
+		//interval_id = setInterval(display_flash,interval_time);	
+		flash_message();
 	}
+
+	function flash_message(){
+		//console.log(timeout_id);
+		var this_num = num;
+		var audio = $('audio').get(this_num);
+		write_english(this_num);
+		audio.play();
+		$('audio').on('ended',function(){
+			timeout_id=setTimeout(function(){
+				write_japanese(this_num);
+				num++;
+				clearTimeout(timeout_id);
+				console.log('aaa');
+				timeout_id=setTimeout(function(){
+					flash_message();
+				},interval_time);
+			},interval_time);
+		});
+	}
+
+	//日本語を表示する
+	function write_japanese(number){
+		var st='position:absolute;bottom:0';
+		disp.html("<div style='"+st+"'>"+d[number][2]+"</div>");
+	}
+	//英語を表示する
+	function write_english(number){
+		var st='position:absolute;bottom:0';
+		disp.html("<div style='"+st+"'>"+d[number][0]+"</div>");
+	}
+
 
 	function data_str_to_array(){
 		var datas = data_str.split('\n');
